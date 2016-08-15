9ba1b44c2bbe679a2fcb8901b594f225ad9747b5
harimanok <codescloudofficial@gmail.com>
2016-08-15 13:50:56 +0545

    populate listview with items

--- Image List/Form1.cs
+++ Image List/Form1.cs
@@ -16,5 +16,54 @@ namespace Image_List
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //set view type to image
+            ListViewImg.View = View.LargeIcon;
+
+            //this imagelist was added in designer mode
+            ListViewImg.LargeImageList = DesignerImageList;
+            int max = DesignerImageList.Images.Count;
+            for (int i = 0; i < max; i++)
+            {
+                //create a group or row
+                ListViewGroup grp = new ListViewGroup(string.Format("Group {0}", i + 1));
+                ListViewImg.Groups.Add(grp);
+                for (int j = 0; j < max; j++)
+                {
+                    //create listview item with text and image, the second argument(index) is index of image in ImageList i.e DesignerImageList here
+                    ListViewItem ImgItem = new ListViewItem(string.Format("Item {0}", j + 1), j);
+                    ImgItem.Group = ListViewImg.Groups[i];
+                    ListViewImg.Items.Add(ImgItem);
+                }
+            }
+        }
+
+        private void BtnUseDialog_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog loadImageDialog = new OpenFileDialog();
+            if (loadImageDialog.ShowDialog(this) != DialogResult.OK) return;
+            ListViewImg.Items.Clear();
+            //we are creating it by code, but this can be done visually in visual studio
+            //add an instance of imagelist and then from properties add the images in imagelist
+            //then finally set the LargeImagelist = yourimagelist
+            ImageList ImagesList = new ImageList();
+            //these properties can be tweaked using visual studio's designer mode 
+            //if you've added it in visual mode
+            ImagesList.ImageSize = new Size(64, 64);
+            ImagesList.Images.Add(Image.FromFile(loadImageDialog.FileName));
+
+            ListViewImg.Groups.Add(new ListViewGroup("First Row"));
+            ListViewImg.Groups.Add(new ListViewGroup("Second Row"));
+            ListViewImg.LargeImageList = ImagesList;
+            //for group based
+            ListViewItem singleItem = new ListViewItem("First", 0);
+            singleItem.Group = ListViewImg.Groups[0];
+            ListViewImg.Items.Add(singleItem);
+            singleItem = new ListViewItem("Second", 0);
+            singleItem.Group = ListViewImg.Groups[1];
+            ListViewImg.Items.Add(singleItem);
+        }
     }
 }
